6f3e546a36f150cf6bc661207a6d701de73886ec
Jonathan Fischoff <jfischoff@yahoo.com>
2017-06-16 01:08:38 -0400

    Better email queue example

--- examples/EmailQueue.hs
+++ examples/EmailQueue.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE RecordWildCards   #-}
 import           Control.Exception.Lifted
 import           Control.Lens
 import           Control.Monad
 import           Control.Monad.IO.Class
-import           Data.Aeson
+import           Data.Aeson as Aeson
 import           Data.Text
 import           Database.Queue
 import           Database.Queue.Main
+import           GHC.Generics
 import           Network.AWS               as AWS
 import           Network.AWS.SES.SendEmail as AWS
 import           Network.AWS.SES.Types     as AWS
@@ -18,20 +22,26 @@ logFailedRequest resp = do
     unless (stat >= 200 && stat < 300) $
       liftIO $ putStrLn $ "SES failed with status: " ++ show stat
 
-makeEmail :: Text -> SendEmail
-makeEmail email
-  = sendEmail "async@example.com"
-              (set dToAddresses [email] destination)
-  $ message (content "Welcome!")
-  $ set bText (Just $ content "You signed up!") AWS.body
+data Email = Email
+  { emailAddress :: Text
+  , emailSubject :: Text
+  , emailBody    :: Text
+  } deriving (Show, Eq, Generic, FromJSON, ToJSON)
+
+makeEmail :: Email -> SendEmail
+makeEmail Email {..}
+  = sendEmail emailAddress
+              (set dToAddresses [emailAddress] destination)
+  $ message (content emailSubject)
+  $ set bText (Just $ content emailBody) AWS.body
 
 main :: IO ()
 main = do
   env <- newEnv Discover
   runResourceT $ runAWS env $ defaultMain "aws-email-queue-consumer" $ \payload _ -> do
-    case pValue payload of
-      String msg -> do
-        resp <- AWS.send $ makeEmail msg
+    case fromJSON $ pValue payload of
+      Aeson.Success email -> do
+        resp <- AWS.send $ makeEmail email
         logFailedRequest resp
-      x -> throwIO $ userError $ "Type error. Wrong type of payload. Expected String got " ++
+      Aeson.Error x -> throwIO $ userError $ "Type error. Wrong type of payload. Expected String got " ++
              show x
