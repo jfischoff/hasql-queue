5441bfac1ea02afeb5fbecad82f200d08fe63c33
Jonathan Fischoff <jfischoff@yahoo.com>
2020-04-19 12:03:59 -0700

    failed and dedequeued implemented

--- src/Hasql/Queue/Session.hs
+++ src/Hasql/Queue/Session.hs
@@ -3,8 +3,9 @@ module Hasql.Queue.Session
   , enqueueNotify
   , dequeue
   , withDequeue
+  , failed
+  , dequeued
   , getCount
-  , Payload (..)
   , PayloadId (..)
   ) where
 import qualified Hasql.Encoders as E
@@ -17,6 +18,7 @@ import           Control.Exception
 import           Control.Monad.IO.Class
 import           Control.Monad (when)
 import           Hasql.Queue.Internal
+import           Data.Maybe
 
 enqueue :: E.Value a -> [a] -> Session ()
 enqueue theEncoder values = do
@@ -119,3 +121,45 @@ withDequeue decoder retryCount f = getEnqueue decoder >>= \case
     liftIO (try $ f pValue) >>= \case
       Left  (e :: SomeException) -> updateStateOnFailure >> liftIO (throwIO e)
       Right x -> pure x
+
+fst3 :: (a, b, c) -> a
+fst3 (x, _, _) = x
+
+snd3 :: (a, b, c) -> b
+snd3 (_, x, _) = x
+
+trd3 :: (a, b, c) -> c
+trd3 (_, _, x) = x
+
+listState :: State -> D.Value a -> Maybe PayloadId -> Int -> Session (PayloadId, [a])
+listState theState valueDecoder mPayloadId count = do
+  let theQuery = [here|
+        SELECT id, value
+        FROM payloads
+        WHERE state = $1
+          AND id > $2
+        ORDER BY id ASC
+        LIMIT $3
+        |]
+      encoder = (fst3 >$< E.param (E.nonNullable stateEncoder))
+             <> (snd3 >$< E.param (E.nonNullable payloadIdEncoder))
+             <> (trd3 >$< E.param (E.nonNullable E.int4))
+
+      decoder =  D.rowList
+              $  (,)
+             <$> D.column (D.nonNullable payloadIdDecoder)
+             <*> D.column (D.nonNullable valueDecoder)
+      theStatement = Statement theQuery encoder decoder True
+
+      defaultPayloadId = fromMaybe initialPayloadId mPayloadId
+
+  idsAndValues <- statement (theState, defaultPayloadId, fromIntegral count) theStatement
+  pure $ case idsAndValues of
+    [] -> (defaultPayloadId, [])
+    xs -> (fst $ last xs, map snd xs)
+
+failed :: D.Value a -> Maybe PayloadId -> Int -> Session (PayloadId, [a])
+failed = listState Failed
+
+dequeued :: D.Value a -> Maybe PayloadId -> Int -> Session (PayloadId, [a])
+dequeued = listState Dequeued
